436291e87ac5b6b6fe44e0dfde51795261ded3b2
molyneauxk93 <joshua.molyneaux@live.com>
2025-05-21 00:16:56 -0400

    Adding updates to pages and components

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
 import './Footer.css'
 
+//bootstrap imports 
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+
 function Footer() {
     return (
-        <div>Footer</div>
+          <div>
+        {/* Beginning of Footer section */ }
+        < hr class="solid" ></hr >
+        <h2>Follow Us on Instagram!</h2>
+
+        <div className="card-section">
+
+            <section className="card">
+
+                <div className="card-header">
+                    <a href="https://www.instagram.com/greatgeorgestreetrx/" target="_blank">
+                        <img src="./src/images/Instagram_icon.png" alt="instagram icon"/>
+                    </a>
+
+                </div>
+
+            </section>
+
+        </div>
+        </div>
     )
 }
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import './Home.css'
 
+// bootstrap imports 
+import Carousel from 'react-bootstrap/Carousel';
+import Button from 'react-bootstrap/Button';
+
 function Home() {
     return (
         <div>
-            Home
+            <Carousel className="carousel">
+                <Carousel.Item>
+                    <img className="carousel-img" src="src/images/20250521_003703000_iOS.jpg" text="First slide" />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img className="carousel-img" src="src/images/20250521_004019000_iOS 1.jpg" text="Second slide" />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img className="carousel-img" src="src/images/20250521_004019000_iOS.jpg" text="Third slide" />
+                </Carousel.Item>
+            </Carousel>
+
+            <p>At the Great George Street Pharmacy, we provide good healthcare with a personalized difference. 
+                With this in mind, our goal is to bring you the same service we provide to our customers in-person to our 
+                online services.</p>
+
+                <div className="button"><Button style={{color:"#DDEB9D"}} size="lg" variant="success">View Products</Button></div>
         </div>
     )
 }
